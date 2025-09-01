d2da2fb7b43b1c67adf52c8269bb3c5e4163f5ef
felipeochoat <101843213+felipeochoat@users.noreply.github.com>
2025-08-31 22:59:55 -0500

    Archivos base

---
+++ lab/src/App.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * @author Alejandro Amaya, Eduardo Ibarra y Felipe Ochoa
+ */
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
